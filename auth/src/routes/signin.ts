2f3e309af9b4a08a8afb2ae33daf3374ca95c16d
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2025-01-25 10:54:19 +0100

    Code Sharing and Reuse between Services

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest, BadRequestError } from '@rawrawtickets/common';
+
 import { Password } from '../services/password';
 import jwt from 'jsonwebtoken';
 
