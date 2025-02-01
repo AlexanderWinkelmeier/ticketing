34eee1f1a5d0305595155acf674210dcfdfad065
Alexander Winkelmeier <R.A.Winkelmeier@hotmail.de>
2025-02-01 09:34:04 +0100

    Connecting to NATS in node.js

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from 'node-nats-streaming';
+import nats from 'node-nats-streaming';
 import { randomBytes } from 'crypto';
+import { TicketCreatedListener } from './events/ticket-created-listener';
 
 console.clear();
 
@@ -15,28 +16,8 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('accounting-service');
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'queue-group-name',
-    options
-  );
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
-    }
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
-// watching for interrupting signal
 process.on('SIGINT', () => stan.close());
-// watching for terminating signal
 process.on('SIGTERM', () => stan.close());
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,19 +1,21 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 
 const stan = nats.connect('ticketing', 'abc', { url: 'http://localhost:4222' });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connects to NATS');
 
-  const data = JSON.stringify({
-    id: '123',
-    title: 'concert',
-    price: 21,
-  });
-
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 21,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
