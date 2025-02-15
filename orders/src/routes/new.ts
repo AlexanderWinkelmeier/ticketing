e843b3a6d075ecf3910f12aff7e2a535615247cf
AlexanderWinkelmeier <r.a.winkelmeier@hotmail.de>
2025-02-15 11:20:01 +0100

    implementing the expiration service and testing

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -13,7 +13,7 @@ import { OrderStatus } from '@rawrawtickets/common';
 import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
 import { natsWrapper } from '../nats-wrapper';
 
-const EXPIRATION_WINDOW_SECONDS = 15 * 60;
+const EXPIRATION_WINDOW_SECONDS = 1 * 60;
 
 const router = express.Router();
 
