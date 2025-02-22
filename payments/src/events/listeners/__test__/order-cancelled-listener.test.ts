01e10039eb97752a090a0ee18ef3092569bd94d5
AlexanderWinkelmeier <r.a.winkelmeier@hotmail.de>
2025-02-22 07:37:21 +0100

    Updates

---
+++ payments/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -0,0 +1,54 @@
+import { OrderCancelledEvent, OrderStatus } from '@rawrawtickets/common';
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCancelledListener } from '../order-cancelled-listener';
+import mongoose from 'mongoose';
+import { Order } from '../../../models/order';
+
+const setup = async () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 10,
+  });
+  await order.save();
+
+  const data: OrderCancelledEvent['data'] = {
+    id: order.id,
+    version: 1,
+    ticket: {
+      id: 'asdfjaä',
+    },
+  };
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+  return { listener, data, msg, order };
+};
+
+it('updates the status of the order', async () => {
+  const { listener, data, msg, order } = await setup();
+  await listener.onMessage(data, msg);
+  const updatedOrder = await Order.findById(order.id);
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it('acks the message', async () => {
+  const { listener, data, msg } = await setup();
+  await listener.onMessage(data, msg);
+  expect(msg.ack).toHaveBeenCalled();
+});
+
+it('does not call ack if the event has a skipped version number', async () => {
+  const { listener, data, msg } = await setup();
+  data.version = 10;
+  try {
+    await listener.onMessage(data, msg);
+  } catch (err) {}
+  expect(msg.ack).not.toHaveBeenCalled();
+});
